e349ff1bb8e9d6290576ac60dc3454a984b37a51
YuanTongCHN <yuantongchn@outlook.com>
2020-12-10 19:42:53 +0800

    version1.0

--- src/indi/yuantongchn/LC7/Solution.java
+++ src/indi/yuantongchn/LC7/Solution.java
@@ -2,6 +2,21 @@ package indi.yuantongchn.LC7;
 
 class Solution {
     public int reverse(int x) {
-
+        if (x == 0) {
+            return 0;
+        }
+        int res = 0;
+        while(x != 0){
+            int unit = x % 10;
+            x = x / 10;
+            if(res > Integer.MAX_VALUE / 10 || res == Integer.MAX_VALUE / 10 && unit > Integer.MAX_VALUE % 10){
+                return 0;
+            }
+            if(res < Integer.MIN_VALUE / 10 || res == Integer.MIN_VALUE / 10 && unit < Integer.MIN_VALUE % 10){
+                return 0;
+            }
+            res = res * 10 + unit;
+        }
+        return res;
     }
 }
