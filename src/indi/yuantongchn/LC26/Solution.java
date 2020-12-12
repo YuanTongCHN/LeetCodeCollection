e1235b8d2f19a36e16cc540e5cff23dba7b5c548
YuanTongCHN <yuantongchn@outlook.com>
2020-12-12 15:03:10 +0800

    version 1.0

---
+++ src/indi/yuantongchn/LC26/Solution.java
@@ -0,0 +1,23 @@
+package indi.yuantongchn.LC26;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int len = nums.length;
+        if(len < 2){
+            return len;
+        }
+        //双指针
+        int fast = 0;
+        int slow = 0;
+        while(fast < len){
+            if(nums[fast] == nums[slow]){
+                fast++;
+            }else{
+                slow++;
+                nums[slow] = nums[fast];
+                fast++;
+            }
+        }
+        return slow + 1;
+    }
+}
